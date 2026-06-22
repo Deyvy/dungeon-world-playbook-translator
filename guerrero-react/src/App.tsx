29a4727423d146aa945be13bf801564aa9081c78
Deyvy <deyvy@example.com>
2026-06-22 17:10:15 +0200

    feat: wire App.tsx with PlaybookData prop and verify compilation

--- guerrero-react/src/App.tsx
+++ guerrero-react/src/App.tsx
@@ -1,9 +1,10 @@
 import PlaybookSheet from './components/PlaybookSheet';
+import { guerreroData } from './data/playbooks/guerreroData';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#e8e4dc] py-8">
-      <PlaybookSheet />
+      <PlaybookSheet data={guerreroData} />
     </div>
   );
 }
